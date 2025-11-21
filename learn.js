78edf5c8bcd6aeaaae427ba56cbeb66a8d4f68af
hamidonlinepro <hamidonline.pro@gmail.com>
2025-11-21 21:19:09 +0330

    Init Stage !

---
+++ learn.js
@@ -0,0 +1,21 @@
+"use strict"
+let name = "John"
+    , Admin = name
+    , earthPlanet
+    , usernameReguest;
+
+const birthday = '18.04.1982';
+
+console.log(Admin);
+
+
+let nam = "Ilya";
+
+console.log(`hello ${1}`); // ?
+
+console.log(`hello ${"nam"}`); // ?
+
+console.log(`hello ${nam}`); // ?
+
+result = prompt("hi", "yes");
+alert(result);
